f348af1d1cb45e2a6b3c497d449396d86d93b05a
r0leksii <108926634+r0leksii@users.noreply.github.com>
2024-05-25 18:51:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function daysLeftBeforeNewYear() {
-    const newYear = new Date('01-01-2024, 00:00:00 UTC+3').getTime();
+    const newYear = new Date('01-01-2025, 00:00:00 UTC+3').getTime();
     let countDownDays;
 
     function getDaysLeftBeforeNewYear() {
